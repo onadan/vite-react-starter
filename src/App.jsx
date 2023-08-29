48aaa72967a8f5535264aefa0a1c9fff5bd9dd31
onadan <onadan45@gmail.com>
2023-08-29 06:19:50 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold">Vite + React + TailwindCss</h1>
+    </>
+  );
+}
+
+export default App;
--- src/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Vite + React + TailwindCss</h1>
-    </>
-  );
-}
-
-export default App;
